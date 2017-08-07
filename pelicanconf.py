1665dc262e2ffbabe26db598df6f87dcc0fe9770
Flavio Percoco <flaper87@gmail.com>
2017-08-07 17:38:45 +0200

    pelican_plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,6 +17,10 @@ TIMEZONE = 'Europe/Rome'
 
 DEFAULT_LANG = u'en'
 
+MARKDOWN = {
+    'output_format': 'html5',
+}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 TAG_FEED_ATOM = 'feeds/%s.atom.xml'
@@ -28,6 +32,11 @@ TWITTER_USERNAME = "flaper87"
 
 DEFAULT_PAGINATION = 5
 TYPOGRIFY = False
+
+PLUGIN_PATHS = [os.path.join(os.path.dirname(__file__), 'pelican-plugins')]
+PLUGINS = [
+    'liquid_tags.youtube',
+]
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
